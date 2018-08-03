8d28c3664785dc8f47ef6dcf8abf06b1f89cd8a0
Mia Jackson <miajackson@Mias-MacBook-Pro.local>
2018-08-03 08:57:12 -0400

    Adding

--- weekly-projects/final-project-2/bookmarklet3.js
+++ weekly-projects/final-project-2/bookmarklet3.js
@@ -38,10 +38,10 @@ function initBookmarklet($) {
                 $('li, ul, a').css('border','2px solid white');
                 $("body").css('background-color', '#a6badb');
                 $('head').append('<link rel="stylesheet" href="https://miajackson.github.io/summer2018-interactive/weekly-projects/final-project-2/practice2-27.css" type="text/css"/>');
-                //$("body").append('<div class="blend"></div>');
-                //$('.blend').css('background-color','rgb(0, 0,' + colorB +')');
+                $("body").append('<div class="blend"></div>');
+                $('.blend').css('background-color','rgb(0, 0,' + colorB +')');
                 //console.log(colorR);
-                //$('body').append(style="background-color:' + colorR + ',' + colorG + ',' + colorB'"></div>'));
+                $('body').append(style="background-color:' + colorR + ',' + colorG + ',' + colorB'"></div>')');
                 $("body").append('<p id="width"></p>');
                 $("body").append('<p id="height"></p>');
 
