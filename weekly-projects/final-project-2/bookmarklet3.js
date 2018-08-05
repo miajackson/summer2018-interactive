44c81682ce032a38a69aa2ff9aa4bd9926fb6240
Mia Jackson <miajackson@Mias-MacBook-Pro.local>
2018-08-05 18:52:35 -0400

    Adding

--- weekly-projects/final-project-2/bookmarklet3.js
+++ weekly-projects/final-project-2/bookmarklet3.js
@@ -104,8 +104,6 @@ if (window.jQuery === undefined || window.jQuery.fn.jquery < version) {
 
                 // Execute a function when the DOM is ready
                 fontSize();
-              });
-              
             })();
 }
 })();
